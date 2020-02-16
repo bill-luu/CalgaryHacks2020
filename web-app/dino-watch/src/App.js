7a682159b66f33ad7a700b94913ae926e714d90a
Paul <paulchenpmc@gmail.com>
2020-02-15 19:35:31 -0700

    Add navbar

--- web-app/dino-watch/src/App.js
+++ web-app/dino-watch/src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ReactGoogleMaps from './ReactGoogleMaps';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import dino from './img/dino.png';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
+    <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title">
+            <img src={dino} alt='U of C Dino Logo' height={60} />
+          </Typography>
+          <Typography variant="h5" className={classes.title}>
+            Dino-Watch
+          </Typography>
+          <Button color="inherit">Add foot traffic</Button>
+        </Toolbar>
+      </AppBar>
       <ReactGoogleMaps> </ReactGoogleMaps>
     </div>
   );
