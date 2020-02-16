56ca3bcf74d85d0145142dbbb94cbe8ba2047597
juan-nieto <nieto.juan15@gmail.com>
2020-02-16 10:51:07 -0700

    added heatmap model that will populate ict, math science, and sscience b when component is rendered on client side

--- web-app/dino-watch/src/ReactGoogleMaps.js
+++ web-app/dino-watch/src/ReactGoogleMaps.js
@@ -8,8 +8,8 @@ import { withFirebase, FirebaseContext } from './firebase';
 /* global google */
 
 class MyMapComponent extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       heatMapData: {
         positions: [
@@ -40,15 +40,52 @@ class MyMapComponent extends React.Component {
   setHeatFlag() {
     this.canSendHeat = true;
   }
+
   componentDidMount() {
+    
     this.props.firebase.hotLocations().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
           const point = new google.maps.LatLng(doc.data().lat, doc.data().lng)
           this._googleMap.heatmap.data.push(point)
+         
       });
-    });
-  }
+    }).then(() => {
+      var currentTime = new Date();
+      console.log(currentTime.getHours())
+      if(currentTime.getHours() >= 9 && currentTime.getHours() <= 16) {
+        const scienceBPoint1 = new google.maps.LatLng({lat: 51.078750, lng: -114.129242})
+        const scienceBPoint2 = new google.maps.LatLng({lat: 51.078986, lng: -114.129894})
+        const scienceBPoint3 = new google.maps.LatLng({lat: 51.078986, lng: -114.129894})
+        const scienceBPoint4 = new google.maps.LatLng({lat: 51.078986, lng: -114.129894})
+        const scienceBPoint5 = new google.maps.LatLng({lat: 51.078986, lng: -114.129894})
+          
+        const mathSciencePoint1 = new google.maps.LatLng({lat: 51.079960, lng: -114.127964})
+        const mathSciencePoint2 = new google.maps.LatLng({lat: 51.079960, lng: -114.127964})
+        const mathSciencePoint3 = new google.maps.LatLng({lat: 51.079960, lng: -114.127964})
+        const mathSciencePoint4 = new google.maps.LatLng({lat: 51.079960, lng: -114.127964})
 
+        const ICTPoint1 = new google.maps.LatLng({lat: 51.079994, lng: -114.129836})
+        const ICTPoint2 = new google.maps.LatLng({lat: 51.079995, lng: -114.129836})
+        const ICTPoint3 = new google.maps.LatLng({lat: 51.079996, lng: -114.129836})
+        const ICTPoint4 = new google.maps.LatLng({lat: 51.079995, lng: -114.129833})
+        
+        this._googleMap.heatmap.data.push(scienceBPoint1)
+        this._googleMap.heatmap.data.push(scienceBPoint2)
+        this._googleMap.heatmap.data.push(scienceBPoint3)
+        this._googleMap.heatmap.data.push(scienceBPoint4)
+        this._googleMap.heatmap.data.push(scienceBPoint5)
+        this._googleMap.heatmap.data.push(mathSciencePoint1)
+        this._googleMap.heatmap.data.push(mathSciencePoint2)
+        this._googleMap.heatmap.data.push(mathSciencePoint3)
+        this._googleMap.heatmap.data.push(mathSciencePoint4)
+        this._googleMap.heatmap.data.push(ICTPoint1)
+        this._googleMap.heatmap.data.push(ICTPoint2)
+        this._googleMap.heatmap.data.push(ICTPoint3)
+        this._googleMap.heatmap.data.push(ICTPoint4)
+      }
+    }
+    );
+  }
   render() {
     return (
     <div style={{ height: '100vh', width: '100%' }}>
